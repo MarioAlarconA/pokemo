b5d7fadecc52e75eddae9f6be4e621b4beb72276
MarioAlarconA <utm23090584@utma.edu.mx>
2025-02-19 10:40:02 -0600

    clase19/02/25

--- app/controllers/pokemon_favorite_controller.py
+++ app/controllers/pokemon_favorite_controller.py
@@ -3,7 +3,41 @@ Elimina
 Modifica la clase del modelo y evitar que se usen metodos indevidos """
 
 from flask import Blueprint, request, jsonify 
+from app.tools.response_manager import ResponseManager
 from app.schemas.poke_fav_schema import PokeFavSchema
+from bson import ObjectId
+from marshmallow import ValidationError
+from app.models.factory import  ModelFactory
+
+bp = Blueprint("pokemon_favorite", __name__, url_prefix="/pokemons-favorites")
+RM = ResponseManager()
+FP_MODEL = ModelFactory.get_model("pokemons_favorites")
+FP_SCHEMA = PokeFavSchema()
+
+@bp.route ("/", methods=["POST"])
+def create ():
+    try:
+        data = request.json
+        data = FP_SCHEMA.validate(data)
+        fp = FP_MODEL.create(data)
+        return RM.success({"_id": fp})
+    except ValidationError as err:
+        print(err)
+        return RM.error("Envia todos lo parametros")
+    
+
+@bp.route("/<string:id>", methods=["DELETE"])
+def delete(id):
+    FP_MODEL.delete(ObjectId(id))
+    return RM.success("Pokemon eliminado con exito")
+
+@bp.route("/<string:user_id>", methods=["GET"])
+def get_all(user_id):
+    data = FP_MODEL.find_all()
+    return RM.success(data)
+
+
+""" from app.schemas.poke_fav_schema import PokeFavSchema
 from marshmallow import ValidationError
 from app.models.factory import ModelFactory
 from bson import ObjectId
@@ -31,4 +65,4 @@ def delete(pokemon_id):
 @bp.route("/get_pokemons/", methods=["GET"])
 def get_pokemon():
     pokemon = pokemon_favorite_model.find_all()
-    return jsonify(pokemon, 200)
+    return jsonify(pokemon, 200) """
--- app/models/pokemon_favorite.py
+++ app/models/pokemon_favorite.py
@@ -1,8 +1,17 @@
 from app import mongo
 from app.models.super_clase import SuperClass
+from bson import ObjectId
 
 class PokemonFavorites(SuperClass):
     def __init__(self):
         super().__init__("pokemon_favorites")
     
+    def update(self, object_id, data):
+        raise NotImplementedError("No se puede actualizar")
     
+    def find_by_id(self, object_id):
+        raise NotImplementedError("No se puede encontrar")
+    
+    def update(self, user_id):
+        data = self.collection.find({"user_id": ObjectId(user_id)})
+        return data
